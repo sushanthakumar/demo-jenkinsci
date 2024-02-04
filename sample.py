a24397fb05eda262b747c28b53e9887917dc6794
sushanth <sushanth.github@gmail.com>
2024-02-05 01:21:30 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,7 +2,7 @@ def SampleFunc():
         print("Sample code to test circle ci feature")
 
 def Sum(a, b, c):
-        return a + b + c
+        return a + b + 
 
 if __name__ == '__main__':
         SampleFunc()
